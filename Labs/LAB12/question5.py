da70e74dd1ef660580dda45f7bdd3405ce6ca831
lordruption <ernestvchan@gmail.com>
2023-09-12 10:33:45 +1200

    done

--- Labs/LAB12/question5.py
+++ Labs/LAB12/question5.py
@@ -1,10 +1,28 @@
 def convert_to_percent(scores_list, out_of):
-    marks = []
-    for i in scores_list:
-        answer = (i / out_of) * 100
-        marks.append(round(answer))  # Round the result to the nearest integer
+    for i in range(len(scores_list)):
+        percentage = (scores_list[i] / out_of) * 100
+        percentage_round = round(percentage, 2)
+        marks[i] = percentage_round
+        
 
-# DO not change code under here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 marks = [25.5, 50, 49, 15.9, 0]
 convert_to_percent(marks, 50)
 print("Marks out of 100:", marks)
+
+
+#Marks out of 100: [51, 100, 98, 32, 0]
