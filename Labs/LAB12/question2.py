c9cd6edc0ac47f6493ca9e3dddc4b3ad640ce99a
lordruption <ernestvchan@gmail.com>
2023-09-11 14:45:45 +1200

    Update question2.py

--- Labs/LAB12/question2.py
+++ Labs/LAB12/question2.py
@@ -1,8 +1,12 @@
+import math
+
 def calculate_frustum_volume(height, radius1, radius2):
-
-
-
-
+    pixheightdividethree = (math. pi * height) / 3
+    rtwooverrone = radius2 / radius1
+    multiply = radius1 * radius2
+    twoovertwo = radius2 / 2
+    volume = pixheightdividethree(rtwooverrone + multiply + twoovertwo)
+    return volume
 
 
 
