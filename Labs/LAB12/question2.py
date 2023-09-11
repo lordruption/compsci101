c4e0ddc14ddc5945e3f20768e4b51822b487b66f
lordruption <ernestvchan@gmail.com>
2023-09-11 14:35:01 +1200

    Create question2.py

---
+++ Labs/LAB12/question2.py
@@ -0,0 +1,18 @@
+def calculate_frustum_volume(height, radius1, radius2):
+
+
+
+
+
+
+
+
+
+
+
+
+height = 10
+radius1 = 8
+radius2 = 4
+volume = calculate_frustum_volume(height, radius1, radius2)
+print("The volume is", volume)
