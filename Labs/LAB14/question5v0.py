59633da52d88f8d67cb433e58a0a8ef6d1c6e37d
lordruption <ernestvchan@gmail.com>
2023-09-21 12:40:42 +1200

    com

---
+++ Labs/LAB14/question5v0.py
@@ -0,0 +1,13 @@
+def process_barchart(filename):
+    text_open = filename.open(filename, "r")
+    text_read = filename.read()
+    print(text_read)
+    text_open.close()
+
+
+
+
+
+
+
+filename = "barchart1.txt"
