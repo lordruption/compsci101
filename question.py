57bc3ac0a3f9d035484fdd4a9e539b42fc31c850
lordruption <ernestvchan@gmail.com>
2023-09-29 01:54:46 +1300

    push

--- question.py
+++ question.py
@@ -1,17 +1,23 @@
 def get_inventory(filename):
-    inventory = {}
-    open_file = open(filename, 'r')
-    file = open_file.readlines()
-    for line in file:
-        parts = line.strip().split('\t')
-        if len(parts) >= 2:
-            item_name = parts[0]
-            item_price = str(parts[-1])
-            inventory[item_name] = item_price
-    open_file.close()
-    del inventory['Item']  
+    inventory = {}  # Initialize an empty dictionary to store the inventory.
+    try:
+        with open(filename, 'r') as file:
+            # Skip the first line (heading).
+            next(file)
+            for line in file:
+                # Split the line into item and price using tab ("\t") as the delimiter.
+                parts = line.strip().split('\t')
+                if len(parts) == 2:
+                    item, price = parts
+                    inventory[item] = float(price)
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
     return inventory
-    
+
+# Example usage:
 filename = "Inventory1.txt"
 inventory = get_inventory(filename)
-print(inventory)
+for item in sorted(inventory):
+    print(f"{item} => ${inventory[item]}")
