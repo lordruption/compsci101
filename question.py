afa20a5457858fb0392a17ae8567c7f5244796e3
lordruption <ernestvchan@gmail.com>
2023-09-29 01:36:42 +1300

    U

--- question.py
+++ question.py
@@ -1,15 +1,17 @@
-def word_analysis(text):
-
-
-
-
-
-
-
-
-
-
-text = "The quick brown fox jumps over the lazy dog"
-word_analysis_dict = word_analysis(text)
-for key in sorted(word_analysis_dict):
-    print(key, word_analysis_dict[key])
+def get_inventory(filename):
+    inventory = {}
+    open_file = open(filename, 'r')
+    file = open_file.readlines()
+    for line in file:
+        parts = line.strip().split('\t')
+        if len(parts) >= 2:
+            item_name = parts[0]
+            item_price = str(parts[-1])
+            inventory[item_name] = item_price
+    open_file.close()
+    del inventory['Item']  
+    return inventory
+    
+filename = "Inventory1.txt"
+inventory = get_inventory(filename)
+print(inventory)
