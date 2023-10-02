90565bde17a4c3e97822796355ced46a639c71c7
lordruption <ernestvchan@gmail.com>
2023-10-03 12:23:55 +1300

    U

--- question.py
+++ question.py
@@ -1,23 +1,44 @@
-def get_inventory(filename):
-    inventory = {}  # Initialize an empty dictionary to store the inventory.
-    try:
-        with open(filename, 'r') as file:
-            # Skip the first line (heading).
-            next(file)
-            for line in file:
-                # Split the line into item and price using tab ("\t") as the delimiter.
-                parts = line.strip().split('\t')
-                if len(parts) == 2:
-                    item, price = parts
-                    inventory[item] = float(price)
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return inventory
+# Define a function named 'update_dictionary' that takes two arguments:
+# 'a_dict', which is a dictionary, and 'target_value', which is the value we want to find and remove from the dictionary.
+def update_dictionary(a_dict, target_value):
+    # Create an empty list named 'keys_to_delete' to store the keys that match the 'target_value'.
+    keys_to_delete = []
 
-# Example usage:
-filename = "Inventory1.txt"
-inventory = get_inventory(filename)
-for item in sorted(inventory):
-    print(f"{item} => ${inventory[item]}")
+    # Iterate through the items (key-value pairs) in the 'a_dict' dictionary.
+    for key, value in a_dict.items():
+        # Check if the 'value' associated with the current 'key' is equal to the 'target_value'.
+        if value == target_value:
+            # If it is, add the 'key' to the 'keys_to_delete' list.
+            keys_to_delete.append(key)
+    
+    # Now that we have identified the keys to delete, iterate through the 'keys_to_delete' list.
+    for key in keys_to_delete:
+        # Delete the key-value pair associated with the 'key' from the 'a_dict' dictionary.
+        del a_dict[key]
+
+    # Finally, return the modified 'a_dict' after removing the key-value pairs with 'target_value'.
+    return a_dict
+
+
+
+
+
+
+
+a_dict = {"a":0, "b":0, "c":1, "d":1, "e":5} #This is the dictionary 
+print("Before:", end=" ") #This prints the original dictionary
+keys = list(a_dict.keys())
+for key in keys:
+    print(f"{key}:{a_dict[key]}", end=" ")
+print()
+update_dictionary(a_dict, 0) #This is where it calls my function
+print("After:", end=" ")
+keys = list(a_dict.keys())
+for key in keys:
+    print(f"{key}:{a_dict[key]}", end=" ")
+print()
+
+
+#Below is the desired output
+#Before: a:0 b:0 c:1 d:1 e:5 
+#After: c:1 d:1 e:5 
