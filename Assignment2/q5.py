a1809c81d467e327ef40b5cd49c505017a90439d
lordruption <ernestvchan@gmail.com>
2023-10-04 15:09:51 +1300

    e

--- Assignment2/q5.py
+++ Assignment2/q5.py
@@ -1,15 +1,11 @@
-#This function Takes input, reprompts if no valid
+# This function takes input, reprompts if not valid
 def get_user_selection():
-    #User Prompt
-    user_selection = input("Enter your selection (1 to 5): ")
-    while user_selection.isdigit() == False:
+    while True:
+        user_input = input("Enter your selection (1 to 5): ")
+        if user_input.isdigit():
+            user_input_int = int(user_input)
+            if user_input_int in range(1, 6):
+                return user_input_int
         print("Please enter a number from 1 to 5")
-        user_selection = input("Enter your selection (1 to 5): ")
-    
-    while user_selection not in range(1, 6):
-        print("Please enter a number from 1 to 5")
-        user_selection = int(input("Enter your selection (1 to 5): "))
-
-    return(user_selection)
 
 print(get_user_selection())
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-valid_input = range(1, 6)
-print(valid_input)
+def print_ox_xo_pattern(number_of_rows, number_of_columns):
+
+
+
+
+
+
+
+#testcase 1
+print_ox_xo_pattern(5, 4)
+'''
+oxox
+xoxo
+oxox
+xoxo
+oxox
+'''
