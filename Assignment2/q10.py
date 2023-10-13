11f6c50df81a11985db895c55f11ed50b48749aa
lordruption <ernestvchan@gmail.com>
2023-10-13 14:21:10 +1300

    e

--- Assignment2/q10.py
+++ Assignment2/q10.py
@@ -1,17 +1,4 @@
-def print_contents(filename):
-    input_stream = open(filename, 'r')
-    content = input_stream.read()
-    input_stream.close()
-    print(content)
-
 def handle_high_scores(high_scores_dict, name, score, high_score_filename):
-    create_file = open(high_score_filename, "w")
-    high_scores_dict[name] = score
-
-
-    
-
-    
 
 
 
@@ -22,6 +9,13 @@ def handle_high_scores(high_scores_dict, name, score, high_score_filename):
 
 
 
+
+
+
+
+
+
+#TestCase
 high_scores_dict = {'Ann': 35, 'Damir': 32, 'Andrew': 33, 'John': 19, 'Barry':29}
 high_score_filename = 'echa347.txt'
 handle_high_scores(high_scores_dict, "Robert", 17, high_score_filename)
